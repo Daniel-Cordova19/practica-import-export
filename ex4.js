ecb1f34fcc93c944dc90a29a02f84d3aea4be1ab
Daniel Cordova <javierdanielcordova@gmail.com>
2023-05-16 13:51:58 +0200

    terminado

--- ex4.js
+++ ex4.js
@@ -1,7 +1,17 @@
 /**
  * 1. Usa Npm para instalar este paquete: https://www.npmjs.com/package/exif . Pedid ayuda para el import, ya que es un poco diferente.
  *
- * 2. Mirando la documentación necesarioa de la misma ágina, usa este módulo de terceros para obtener la información EXIF de DSCN0010.jpg y mostrarlo por consola
+ * 2. Mirando la documentación necesarioa de la misma página, usa este módulo de terceros para obtener la información EXIF de DSCN0010.jpg y mostrarlo por consola
  *
+
+
  * 3. ¿Serías capaz de ubicar la imagen a partir de las coordenadas GPS que arroja la consola?
  */
+
+
+import exif from "jpeg-exif";
+ 
+const filePath = "./DSCN0010.jpg";
+const data = exif.parseSync(filePath);
+ 
+console.log(data);
